2e4763ab213d3806f4f439796f9994f5f75bf7db
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-08 13:01:02 +0200

    add employee component

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -3,7 +3,7 @@ import { Button, Flex } from '@chakra-ui/react'
 export default function NavigationBar() {
   return (
     <Flex
-      className="w-full items-center gap-6  border-b px-4 py-10 text-lg"
+      className="w-full items-center gap-6 border-b px-4 py-10 text-lg"
       style={{ height: '7.5vh' }}
     >
       <Button isActive>Employees</Button>
