2fa0bc9a9d24fe57f8cbcca4ac1f3d72f2b45121
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-10 14:03:28 +0200

    add search input in navigation bar component

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,14 +1,44 @@
-import { Button, Flex } from '@chakra-ui/react'
+import {
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react'
+import React from 'react'
+import { SearchIcon } from 'lucide-react'
 
 export default function NavigationBar() {
   return (
     <Flex
-      className="w-full items-center gap-6 border-b px-4 py-10 text-lg"
+      className="w-full items-center justify-between border-b px-4 py-10 text-lg"
       style={{ height: '7.5vh' }}
     >
-      <Button isActive>Employees</Button>
-      <Button>Departments</Button>
-      <Button>Projects</Button>
+      <Flex className="gap-6">
+        <Button isActive>Employees</Button>
+        <Button>Departments</Button>
+        <Button>Projects</Button>
+      </Flex>
+
+      <Flex className="w-1/3">
+        <InputGroup>
+          <InputRightElement children={<SearchIcon />} pointerEvents="none" />
+
+          <Input
+            autoFocus
+            type="text"
+            className="m-0 bg-transparent pl-2"
+            placeholder="Search..."
+            size="md"
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                if ((e.target as HTMLInputElement).value !== searchQuery) {
+                }
+              }
+            }}
+          />
+        </InputGroup>
+      </Flex>
     </Flex>
   )
 }
