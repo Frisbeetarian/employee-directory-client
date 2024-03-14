a821fecb46bc1c19dd14b7fbc1f55edcdde150eb
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-14 12:07:47 +0200

    correctly reference env variables for api endpoint

--- store/api/searchAPISlice.ts
+++ store/api/searchAPISlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const searchAPISlice = createApi({
   reducerPath: 'api/search',
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:4020/api`,
+    baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   }),
   tagTypes: ['Search'],
   endpoints: (builder) => ({
